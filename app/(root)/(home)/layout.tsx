7e276696cc1ca7115728cbc2d878ee509208edf1
Justin <justinguerrerowork@gmail.com>
2024-09-12 10:46:34 -0400

    initial

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react'
+
+const HomeLayout = ({children}: {children:ReactNode}) => {
+  return (
+    <main className = "relative">
+        NAVBAR
+        <div className = "flex">
+            Sidebar
+            <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28">
+
+            </section>
+        </div>
+        {children}
+        FOOTER
+    </main>
+  )
+}
+
+export default HomeLayout
