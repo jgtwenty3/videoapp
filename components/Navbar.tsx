0db9b1638f6ac1180ae6ecd41960eb111a7e77b9
Justin <justinguerrerowork@gmail.com>
2024-09-15 21:25:16 -0400

    m

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => {
   return (
     <nav className = "flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href = "/" className = "flex items-center gap-1">
-      
         <p className = "text-[26px] font-extrabold text-white">HYVID</p>
       </Link>
       <div className = "flex-between gap-5">
